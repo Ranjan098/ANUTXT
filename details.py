563d3422c19a75d43e73a68eb62419d88cf773e8
Ranjan098 <144373098+Ranjan098@users.noreply.github.com>
2023-11-09 10:14:27 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27881923
-api_hash = "79abda5e46a51fc0dce1313f2548ce19"
-bot_token = "6005960715:AAHB9INP_OzN_b4gZzKeRMOM--X8H6wfkAc"
+api_id = 12606917
+api_hash = "f25113b8c17dca6fa7abda53a86bd4f7"
+bot_token = "6914844052:AAH4dwEH2UWs3aJxCWhHtBvlzv1ODOSuMpY"
 
